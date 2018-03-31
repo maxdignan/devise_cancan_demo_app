b4788afe866718c5f6556aead77e9b67c629003d
mdignan <maxdignan@gmail.com>
2018-03-31 13:43:21 -0400

    rails generate devise:install rails g devise User rake db:migrate rails generate devise:views
    Users have tasks and can sign up and sign in securely. But everyone can
    see everyone's tasks!!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tasks
+
+  root 'users#sign_in'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
